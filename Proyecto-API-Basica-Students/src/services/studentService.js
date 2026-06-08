6c999b911ac61c69a1bc99a2ab670f1eced79ed6
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2026-06-08 08:56:49 -0400

    delete student

--- Proyecto-API-Basica-Students/src/services/studentService.js
+++ Proyecto-API-Basica-Students/src/services/studentService.js
@@ -91,9 +91,12 @@ export function deleteStudentLogicallyById(studentId){
             message: `Not found student with id ${studentId} to update`
         };
     }
-    studentList[pos].active = 0;
+
+    const deletedStudent = studentList[pos];
+    studentList.splice(pos, 1);
+
     return {
         success: true,
-        data: studentList[pos]
+        data: deletedStudent
     };
 }
